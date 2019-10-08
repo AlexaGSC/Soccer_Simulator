3f94255adf31811b90a4dcf38693d438278c9609
Alexa Sanchez <alexsc167@gmail.com>
2019-10-08 20:44:04 +0100

    soccer simulator not ended

---
+++ src/js/index.js
@@ -0,0 +1,24 @@
+/* index.js */
+const button = document.querySelector('.button');
+const content = document.querySelector('.content');
+const playersA = ['Barcelona', 'Madrid', 'Valencia', 'Getafe'];
+const playersB = ['Barcelona', 'Madrid', 'Valencia', 'Getafe'];
+
+
+function puntuacion() {
+
+  const A = Math.round(Math.random() * playersA.length);
+  const B = Math.round(Math.random() * playersB.length);
+  const golesA = Math.round(Math.random() * 10);
+  const golesB = Math.round(Math.random() * 10);
+  const playerA = playersA[A];
+  const playerB = playersB[B];
+
+  content.innerHTML = playerA + '' + golesA + '' + golesB + '' + playerB;
+
+}
+
+button.addEventListener('click', puntuacion);
+
+
+
